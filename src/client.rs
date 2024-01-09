759c68c5fd6276edda7d447276acf0e6d4d43524
Josh <jks.hardy@gmail.com>
2024-01-09 20:33:05 +0000

    removing redundant config struct

--- src/client.rs
+++ src/client.rs
@@ -2,24 +2,24 @@ use ethers::middleware::SignerMiddleware;
 use ethers::prelude::{Http, Provider};
 use ethers_signers::{HDPath, Ledger};
 
-pub struct LedgerClientConfig {
-    pub ledger_derivation_path: Option<usize>,
-    pub chain_id: u64,
-    pub rpc_url: String,
-}
+pub struct LedgerClientConfig {}
 
 pub struct LedgerClient {
     pub client: SignerMiddleware<Provider<Http>, Ledger>,
 }
 
 impl LedgerClient {
-    pub async fn new(config: LedgerClientConfig) -> anyhow::Result<Self> {
+    pub async fn new(
+        ledger_derivation_path: Option<usize>,
+        chain_id: u64,
+        rpc_url: String,
+    ) -> anyhow::Result<Self> {
         let wallet = Ledger::new(
-            HDPath::LedgerLive(config.ledger_derivation_path.unwrap_or(0)),
-            config.chain_id,
+            HDPath::LedgerLive(ledger_derivation_path.unwrap_or(0)),
+            chain_id,
         )
         .await?;
-        let provider = Provider::<Http>::try_from(config.rpc_url.clone())?;
+        let provider = Provider::<Http>::try_from(rpc_url.clone())?;
         let client = SignerMiddleware::new_with_provider_chain(provider, wallet).await?;
         Ok(Self { client })
     }
