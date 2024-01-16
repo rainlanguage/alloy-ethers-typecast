dfabc962bed969d2b988968b651146d8fa73a3fb
Josh <jks.hardy@gmail.com>
2024-01-16 13:11:26 +0000

    splitting into separate files

--- src/transaction/read.rs
+++ src/transaction/read.rs
@@ -1,86 +1,10 @@
 use crate::alloy_u64_to_ethers;
 use crate::request_shim::{AlloyTransactionRequest, TransactionRequestShim};
-use alloy_primitives::{Address, U256, U64};
+use alloy_primitives::{Address, U64};
 use alloy_sol_types::SolCall;
 use derive_builder::Builder;
-use ethers::middleware::SignerMiddleware;
 use ethers::providers::{JsonRpcClient, Middleware, Provider, ProviderError};
-use ethers::signers::Signer;
 use ethers::types::transaction::eip2718::TypedTransaction;
-use ethers::types::TransactionReceipt;
-use ethers::utils::hex;
-use tracing::info;
-
-#[derive(Builder)]
-pub struct WriteContractParameters<C: SolCall + Default> {
-    pub call: Option<C>,
-    pub address: Option<Address>,
-    #[builder(setter(into, strip_option))]
-    pub gas: Option<U256>,
-    #[builder(setter(into, strip_option))]
-    pub gas_price: Option<U256>,
-    #[builder(setter(into, strip_option))]
-    pub max_fee_per_gas: Option<U256>,
-    #[builder(setter(into, strip_option))]
-    pub max_priority_fee_per_gas: Option<U256>,
-    #[builder(setter(into, strip_option))]
-    pub nonce: Option<U256>,
-    #[builder(setter(into, strip_option))]
-    pub value: Option<U256>,
-}
-
-pub struct WritableClient<M: Middleware, S: Signer>(SignerMiddleware<M, S>);
-
-impl<M: Middleware, S: Signer> WritableClient<M, S> {
-    // Create a new WriteContract instance, passing a client
-    pub fn new(client: SignerMiddleware<M, S>) -> Self {
-        Self(client)
-    }
-
-    // Executes a write function on a contract.
-    pub async fn write<C: SolCall + Default>(
-        self,
-        parameters: WriteContractParameters<C>,
-    ) -> anyhow::Result<TransactionReceipt> {
-        let data = parameters
-            .call
-            .ok_or(anyhow::anyhow!("No call provided"))?
-            .abi_encode();
-
-        let transaction_request = AlloyTransactionRequest::new()
-            .with_to(parameters.address)
-            .with_data(Some(data))
-            .with_gas(parameters.gas)
-            .with_max_fee_per_gas(parameters.max_fee_per_gas)
-            .with_max_priority_fee_per_gas(parameters.max_priority_fee_per_gas)
-            .with_nonce(parameters.nonce)
-            .with_value(parameters.value);
-
-        let ethers_transaction_request = transaction_request.to_eip1559();
-
-        let pending_tx = self
-            .0
-            .send_transaction(ethers_transaction_request, None)
-            .await
-            .map_err(|err| anyhow::anyhow!("{}", err))?;
-
-        info!("Transaction submitted. Awaiting block confirmations...");
-
-        let tx_confirmation = pending_tx.confirmations(4).await?;
-
-        let tx_receipt = match tx_confirmation {
-            Some(receipt) => receipt,
-            None => return Err(anyhow::anyhow!("Transaction failed")),
-        };
-
-        info!("Transaction Confirmed");
-        info!(
-            "✅ Hash : 0x{}",
-            hex::encode(tx_receipt.transaction_hash.as_bytes())
-        );
-        Ok(tx_receipt)
-    }
-}
 
 #[derive(Builder)]
 pub struct ReadContractParameters<C: SolCall> {
