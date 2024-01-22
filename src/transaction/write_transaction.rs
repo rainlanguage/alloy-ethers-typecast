81b1513f2da876a276e284269f439333382c0338
Matt Gabrenya <matt@buildyourweb.app>
2024-01-21 22:42:20 -0700

    test: basic test to excercise WriteTransaction execute

--- src/transaction/write_transaction.rs
+++ src/transaction/write_transaction.rs
@@ -88,3 +88,73 @@ impl<M: Middleware, S: Signer, C: SolCall + Clone, F: Fn(WriteTransactionStatus<
         (self.status_changed)(status);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::transaction::mock_middleware::{MockJsonRpcClient, MockMiddleware};
+    use crate::transaction::WriteContractParametersBuilder;
+
+    use super::*;
+    use alloy_primitives::{Address, U256};
+    use alloy_sol_types::sol;
+    use ethers::core::rand::thread_rng;
+    use ethers::providers::Provider;
+    use ethers::signers::LocalWallet;
+    use ethers::types::{Bytes, H160};
+    use tracing_subscriber;
+    use tracing_subscriber::FmtSubscriber;
+
+    sol! {
+       function foo(uint256 a, uint256 b) external view returns (Foo);
+
+        struct Foo {
+            uint256 bar;
+            address baz;
+        }
+    }
+
+    #[tokio::test]
+    async fn test_write_transaction() -> anyhow::Result<()> {
+        // Create a mock transport
+        let mock_transport = MockJsonRpcClient::new();
+        // Create a Provider instance
+        let provider = Provider::new(mock_transport);
+        // Create a mock middleware
+        let mut mock_middleware = MockMiddleware::new(provider)?;
+        // Create a mock wallet
+        let wallet = LocalWallet::new(&mut thread_rng());
+
+        // Create a WriteContractParameters instance
+        let parameters = WriteContractParametersBuilder::default()
+            .call(fooCall {
+                a: U256::from(42), // these could be anything, the mock provider doesn't care
+                b: U256::from(10),
+            })
+            .address(Address::repeat_byte(0x22))
+            .build()?;
+
+        // Create a mock response
+        mock_middleware.assert_next_data(Bytes::from(parameters.call.abi_encode()));
+        mock_middleware.assert_next_to(H160::repeat_byte(0x22));
+
+        // Finally create a client SignerMiddleware instance
+        let client = SignerMiddleware::new(mock_middleware, wallet);
+
+        // Create a WritableClient instance with the mock client
+        WriteTransaction::new(client, parameters, 4, |_| {})
+            .execute()
+            .await?;
+
+        Ok(())
+    }
+
+    #[allow(dead_code)]
+    fn setup_tracing() {
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(tracing::Level::DEBUG)
+            .finish();
+
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("Failed to set tracing subscriber");
+    }
+}
