d0d56825935dd7e0dd89c1a44b8607f90b6719fa
Matt Gabrenya <matt@buildyourweb.app>
2024-03-29 13:02:48 -0700

    feat: fn to list ledger address for a range of derivation indexes

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,10 @@
+use crate::ethers_address_to_alloy;
 use crate::gas_fee_middleware::{GasFeeMiddleware, GasFeeMiddlewareError, GasFeeSpeed};
+use alloy_primitives::Address;
 use ethers::middleware::SignerMiddleware;
 use ethers::prelude::{Http, Provider};
-use ethers::signers::{HDPath, Ledger};
+use ethers::signers::{HDPath, Ledger, LedgerError};
+use std::iter::zip;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -14,6 +17,13 @@ pub enum LedgerClientError {
     CreateLedgerClientMiddlewareError(String),
     #[error(transparent)]
     GasFeeMiddlewareError(#[from] GasFeeMiddlewareError<Provider<Http>>),
+    #[error(transparent)]
+    LedgerError(#[from] LedgerError),
+}
+
+pub enum DerivationPathType {
+    LedgerLive,
+    Legacy,
 }
 
 pub struct LedgerClient {
@@ -21,18 +31,22 @@ pub struct LedgerClient {
 }
 
 impl LedgerClient {
+    /// Initialize a new Ledger connection
+    async fn init_ledger(
+        derivation: Option<HDPath>,
+        chain_id: u64,
+    ) -> Result<Ledger, LedgerClientError> {
+        Ok(Ledger::new(derivation.unwrap_or(HDPath::LedgerLive(0)), chain_id).await?)
+    }
+
+    /// Initialize a new Ledger Signer with all desired middleware
     pub async fn new(
-        ledger_derivation_path: Option<usize>,
+        derivation: Option<HDPath>,
         chain_id: u64,
         rpc_url: String,
         gas_fee_speed: Option<GasFeeSpeed>,
     ) -> Result<Self, LedgerClientError> {
-        let wallet = Ledger::new(
-            HDPath::LedgerLive(ledger_derivation_path.unwrap_or(0)),
-            chain_id,
-        )
-        .await
-        .map_err(|err| LedgerClientError::CreateLedgerClientDeviceError(err.to_string()))?;
+        let wallet = Self::init_ledger(derivation, chain_id).await?;
         let provider = Provider::<Http>::try_from(rpc_url.clone())
             .map_err(|err| LedgerClientError::CreateLedgerClientProviderError(err.to_string()))?;
         let gas_fee_middleware = GasFeeMiddleware::new(provider, gas_fee_speed)?;
@@ -40,5 +54,36 @@ impl LedgerClient {
             .await
             .map_err(|err| LedgerClientError::CreateLedgerClientMiddlewareError(err.to_string()))?;
         Ok(Self { client })
+    }
+
+    /// List all addresses derived from a range of derivation indexes for a given derivation path type
+    pub async fn list_derivation_addresses(
+        chain_id: u64,
+        derivation_path_type: DerivationPathType,
+        index_min: usize,
+        index_max: usize,
+    ) -> Result<Vec<(Address, usize)>, LedgerClientError> {
+        let wallet = Self::init_ledger(None, chain_id).await?;
+        let all_indexes: Vec<usize> = (index_min..index_max).collect();
+        let all_derivations: Vec<HDPath> = match derivation_path_type {
+            DerivationPathType::LedgerLive => all_indexes
+                .clone()
+                .into_iter()
+                .map(HDPath::LedgerLive)
+                .collect(),
+            DerivationPathType::Legacy => all_indexes
+                .clone()
+                .into_iter()
+                .map(HDPath::Legacy)
+                .collect(),
+        };
+
+        let mut all_addresses: Vec<Address> = vec![];
+        for derivation in all_derivations {
+            let address = wallet.get_address_with_path(&derivation).await?;
+            all_addresses.push(ethers_address_to_alloy(address));
+        }
+
+        Ok(zip(all_addresses, all_indexes).collect())
     }
 }
