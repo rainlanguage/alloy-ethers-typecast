0e7ffb929a6604855cb109c4b70238c907f19ea2
highonhopium <jks.hardy@gmail.com>
2024-12-02 22:13:51 +0000

    build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let file_path = "./contracts/IMulticall3.sol";
+    let manifest_dir =
+        std::env::var("CARGO_MANIFEST_DIR").expect("Failed to get manifest directory");
+    let contracts_dir = format!("{}/contracts", manifest_dir);
+    let file_path = format!("{}/IMulticall3.sol", contracts_dir);
+
     let file_url = "https://raw.githubusercontent.com/mds1/multicall/1d08785d1c2e817105ca34c7c7b126297a258017/src/interfaces/IMulticall3.sol";
-    std::fs::create_dir_all("./contracts").expect("failed to create 'contracts' directory");
+
+    std::fs::create_dir_all(&contracts_dir).expect("failed to create 'contracts' directory");
     std::process::Command::new("curl")
-        .args(["-s", "-o", file_path, file_url])
+        .args(["-s", "-o", &*file_path, file_url])
         .status()
         .expect("Failed to get IMulticall3 contract abi!")
         .success()
