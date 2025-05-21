0a2250504c2cfa1c352acc90d4046fbc183180de
0xgleb <gleb.dianov@protonmail.com>
2025-05-21 18:14:54 +0400

    upd nix flake

--- flake.nix
+++ flake.nix
@@ -2,15 +2,14 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rainix.url = "github:rainprotocol/rainix";
+    rainix.url =
+      "github:rainprotocol/rainix?rev=4e87c16f3abcb4e9e309609f452669f5a85bb558";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, flake-utils, rainix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      {
-        packages = rainix.packages.${system};
-        devShells = rainix.devShells.${system};
-      }
-    );
+  outputs = { self, flake-utils, rainix }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = rainix.packages.${system};
+      devShells = rainix.devShells.${system};
+    });
 }
